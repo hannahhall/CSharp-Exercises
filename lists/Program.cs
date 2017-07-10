25118724c64ef9cae897be6c5bc14addd8511958
Hannah Hall <hannahkhall13@gmail.com>
2017-07-10 11:06:10 -0500

    list exercise

---
+++ lists/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>(){"Mercury", "Mars"};
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+            List<string> lastPlanets = new List<string>(){"Uranus", "Neptune"};
+            planetList.AddRange(lastPlanets);
+            planetList.Insert(1, "Venus");
+            planetList.Insert(2, "Earth");
+            List<string> rockyPlanets = planetList.GetRange(0, 4);
+
+            foreach (string planet in planetList)
+            {
+                Console.WriteLine(planet);
+            }
+
+             foreach (string planet in planetList)
+            {
+                Console.WriteLine(planet);
+            }
+            
+        }
+    }
+}
