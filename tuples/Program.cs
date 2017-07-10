0e32dd1e2b9ff0dcf702d4a3ddec948d3600e2c5
Hannah Hall <hannahkhall13@gmail.com>
2017-07-10 11:06:25 -0500

    tuple exercise

---
+++ tuples/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<(string product, double amount, int quantity)> transactions = new List<(string, double, int)>();
+            transactions.Add(("cookies", 1.23, 55));
+            transactions.Add(("tacos", 5.53, 60));
+            transactions.Add(("pizza", 6.23, 85));
+            transactions.Add(("cheese", 4.23, 43));
+            transactions.Add(("ice cream", 8.34, 21));
+
+            int itemsSold = 0;
+            double revenue= 0.0;
+
+            foreach ((string product, double amount, int quantity) t in transactions)
+            {
+                // Logic goes here to look up quantity and amount in each transaction
+                itemsSold += t.quantity;
+                revenue += t.amount;
+            }
+            Console.WriteLine($"Items sold today: {itemsSold}\nTotal Revenue: {revenue}");
+
+        }
+    }
+}
