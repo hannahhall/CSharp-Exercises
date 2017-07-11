fad476b1a769352c0c25cf4c62237d49e9099ba0
Hannah Hall <hannahkhall13@gmail.com>
2017-07-11 09:51:59 -0500

    hashsets

---
+++ hashsets/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace hashsets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> Showroom = new HashSet<string>();
+            Showroom.Add("Rogue");
+            Showroom.Add("Wrangler");
+            Showroom.Add("Camaro");
+            Showroom.Add("Compass");
+            Console.WriteLine($"There are {Showroom.Count} cars in the showroom");
+            Showroom.Add("Camaro");
+            Console.WriteLine($"There are still {Showroom.Count} cars in the showroom");
+
+            HashSet<string> UsedLot = new HashSet<string>(){"Jalupa", "El Camino"};
+            Showroom.UnionWith(UsedLot);
+            // Showroom.Remove("Camaro");
+
+            HashSet<string> Junkyard = new HashSet<string>{"Camaro", "Lemon", "Compass", "Tacoma"};
+            // Junkyard.IntersectWith(Showroom);
+            // foreach(string car in Junkyard)
+            // {
+            //     Console.WriteLine(car);
+            // }
+
+            Showroom.UnionWith(Junkyard);
+            Showroom.Remove("Lemon");
+            foreach(string car in Showroom)
+            {
+                Console.WriteLine(car);
+            }
+            
+            
+            
+        }
+    }
+}
