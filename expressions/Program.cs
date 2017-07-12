0fdeb04d962b48b784c3e7b09ba762d5a70c0aa1
Hannah Hall <hannahkhall13@gmail.com>
2017-07-12 11:08:53 -0500

    expression exercise

---
+++ expressions/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Predators = new List<string>(){
+                "Birds",
+                "Frogs",
+                "Lizards",
+                "Snacks"
+            };
+
+            List<string> Prey = new List<string>(){
+                "Leaves",
+                "Flowers",
+                "Smaller Bugs?",
+            };
+
+            Bug LadyBug = new Bug("Carol", "Lady Bug", Predators, Prey);
+
+            Console.WriteLine($"{LadyBug.FormalName} eats {LadyBug.PreyList()}. It's predators are {LadyBug.PredatorList()}. {LadyBug.Eat("Leaves")} {LadyBug.Eat("beer")}");
+        }
+    }
+}
