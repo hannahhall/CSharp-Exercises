f42a3facf6e3e489ec5b96ee30087bcee80544c0
Hannah Hall <hannahkhall13@gmail.com>
2017-07-12 13:06:54 -0500

    dictionary

---
+++ family-dictionary/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace family_dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+
+            myFamily.Add("sister", new Dictionary<string, string>(){ 
+                {"name", "Dara"},
+                {"age", "24"}
+            });
+
+            myFamily.Add("dad", new Dictionary<string, string>(){
+                {"name", "Thomas"},
+                {"age", "55"}
+            });
+
+            myFamily.Add("mom", new Dictionary<string, string>(){
+                {"name", "Jamie"},
+                {"age", "56"}
+            });
+
+            foreach (var person in myFamily)
+            {
+                Console.WriteLine($"{person.Value["name"]} is my {person.Key} and is {person.Value["age"]}");
+                
+            }
+            
+        }
+    }
+}
